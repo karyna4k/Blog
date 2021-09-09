047c9747b44c2106fba3cb43f9d016f1d0f8a3b8
Karyna Kananovich <karyna.kananovich@gmail.com>
2021-09-09 15:31:39 +0300

    feat: learn dynamic routes

---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Post id is: {{ $route.params.id }}</h1>
+</template>
+
+<script>
+export default {
+  validate ({ params }) {
+    // Number
+
+    return /^\d+$/.test(params.id);
+  }
+};
+</script>
---
+++ pages/posts/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="posts">
+    <input v-model="postId" type="text">
+    <button @click="loadPost">
+      Get post
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      postId: ""
+    };
+  },
+  methods: {
+    loadPost () {
+      this.$router.push(`/posts/${this.postId}`);
+    }
+  }
+};
+</script>
