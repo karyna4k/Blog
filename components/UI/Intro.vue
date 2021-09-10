2b5a50f85c430fc968f8a1b89db62837a2ae59dc
Karyna Kananovich <karyna.kananovich@gmail.com>
2021-09-10 16:23:45 +0300

    feat: add Intro component

---
+++ components/UI/Intro.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+section.intro
+  .container
+    h2 {{ title }}
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.intro {
+  width: 100%;
+  margin: 32px 0;
+  padding: 32px;
+  color: #fff;
+  background-color: #4b40e3;
+  text-align: center;
+
+  h2 {
+    font-size: 24px;
+  }
+}
+</style>
