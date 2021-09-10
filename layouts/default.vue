184a52ad0b50709f9beb453e386887dc758971ed
Karyna Kananovich <karyna.kananovich@gmail.com>
2021-09-10 11:06:39 +0300

    feat: add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+.wrapper
+  Header
+  nuxt
+</template>
+
+<script>
+import Header from "@/components/system/Header.vue";
+
+export default {
+  components: { Header }
+};
+</script>
