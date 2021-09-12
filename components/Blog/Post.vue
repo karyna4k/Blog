6ec95bb408314242a71267602c79cbba985bbcdc
Karyna Kananovich <karyna.kananovich@gmail.com>
2021-09-12 18:41:03 +0300

    feat: add Post component

---
+++ components/Blog/Post.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+section.post
+  .container
+    .post__header
+      img(:src="post.img", :alt="post.title")
+      h1.title {{ post.title }}
+      p {{ post.descr }}
+    .post__body
+      p {{ post.content }}
+</template>
+
+<script>
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
