078ef49357bdd65ee342619a0838aafd1e5885a9
Karyna Kananovich <karyna.kananovich@gmail.com>
2021-09-12 18:41:34 +0300

    feat: add NewComment component

---
+++ components/Comments/NewComment.vue
@@ -0,0 +1,49 @@
+<template lang="pug">
+section.new-comment
+  .container
+    h2.title New comment:
+    form.new-comment__form(@submit.prevent="onSubmit")
+      label Name:
+      input(v-model="comment.name")
+      label Message:
+      textarea(v-model="comment.text")
+      .new-comment__control
+        button.btn.btnPrimary Submit
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      comment: {
+        name: "",
+        text: "",
+      },
+    };
+  },
+  methods: {
+    onSubmit () {
+      console.log(this.comment);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.new-comment {
+  h2 {
+    text-align: center;
+  }
+
+  &__form {
+    max-width: 600px;
+    margin: 30px auto;
+  }
+
+  &__control {
+    display: flex;
+    justify-content: center;
+    margin: 32px;
+  }
+}
+</style>
