0db7f9dcebbb3b7720fb81baeb190c739e271f2a
Karyna Kananovich <karyna.kananovich@gmail.com>
2021-09-14 12:48:28 +0300

    fix; change Header component

--- components/system/Header.vue
+++ components/system/Header.vue
@@ -3,7 +3,8 @@ header
   .navbar
     .container
       .navbar-content
-        .logo Blog
+        .logo
+          nuxt-link(to="/") Blog
         ul.navbar-list
           li.navbar-item(v-for="link in links", :key="link.title")
             nuxt-link.navbar-link(:title="link.title", :to="link.url") {{ link.title }}
@@ -14,7 +15,7 @@ export default {
   data () {
     return {
       links: [
-        { title: "Home", url: "/" },
+        { title: "Blog", url: "/blog" },
         { title: "About", url: "/about" }
       ]
     };
@@ -23,9 +24,15 @@ export default {
 </script>
 
 <style lang="scss">
+.logo a {
+  color: #333;
+  font-weight: 700;
+}
+
 .navbar-link {
   &.nuxt-link-exact-active {
     color: #4b40e3;
+    font-weight: 700;
   }
 }
 </style>
