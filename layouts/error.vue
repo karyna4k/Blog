ec51f6fc7dc19cfaf26eba648a32f8f0de1312a1
Karyna Kananovich <karyna.kananovich@gmail.com>
2021-09-14 12:48:12 +0300

    feat: add 404 page

---
+++ layouts/error.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+section.not-found
+  .container
+    .not-found__wrapper
+      h1.title THE PAGE YOU WERE LOOKING FOR DOESN'T EXIST.
+      p You may have mistyped the address or the page may have moved.
+      nuxt-link(to="/") Go to homepage
+</template>
+
+<style lang="scss">
+.not-found {
+  &__wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 80vh;
+
+    p {
+      margin: 16px 0;
+    }
+  }
+}
+</style>
