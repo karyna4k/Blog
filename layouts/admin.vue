fc61fe89462fdca0afe36be6fffa4094221de969
Karyna Kananovich <karyna.kananovich@gmail.com>
2021-09-14 15:41:17 +0300

    feat: add admin layout

---
+++ layouts/admin.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+.wrapper
+  Header
+  .wrapper-content.wrapper-content--fixed
+    Intro(title="Admin page")
+      nuxt-link.link.linkWhite(to="/admin") Admin
+    nuxt
+</template>
+
+<script>
+import Header from "~/components/system/Header.vue";
+
+export default {
+  components: { Header }
+};
+</script>
